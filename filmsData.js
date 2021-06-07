3275db070bb123d299a92889f717e6042c7603f9
Jinhee Yang <78382611+Jina-123@users.noreply.github.com>
2021-06-07 20:59:46 +0200

    Create filmsData.js

---
+++ filmsData.js
@@ -0,0 +1,44 @@
+var filmsData = [
+    {
+        title : "Intouchables",
+        image : "images/intouchables.jpg",
+        text : `L'histoire est inspirée de la vie de Philippe Pozzo di Borgo (auteur du livre Le Second Souffle en 2001),
+        tétraplégique depuis 1993, et de sa relation avec Abdel Yasmin Sellou, son aide à domicile,
+        dont les rôles sont tenus respectivement par les acteurs François Cluzet et Omar Sy.
+        Le générique de fin indique que 5 % des bénéfices réalisés par le film sont reversés à une association
+        pour les personnes paralysées,Simon de Cyrène1, fondée par Laurent de Cherisey.`,
+    },
+    { 
+        title :  "Babysitting",
+        image : "images/babysitting.jpg", 
+        text : `Faute de baby-sitter pour le week-end, Marc Schaudel confie son fils Rémy à Franck, son employé, un type sérieux selon lui.
+        Sauf que Franck a 30 ans ce soir et que Rémy est un sale gosse capricieux. Au petit matin,
+        Marc et sa femme Claire sont réveillés par un appel de la police qui les informe que Rémy et Franck ont disparu !
+        Au milieu de leur maison saccagée, la police retrouve une caméra. Marc et Claire découvrent, hallucinés,
+        les images tournées pendant la soirée.`,
+    },
+    {
+        title : "Les Choristes",
+        image : "images/choristes.jpg",
+        text : `Un chef d'orchestre se souvient du professeur de musique qui l'a influencé et replonge dans le passé.
+        En 1949, Clément Mathieu, professeur de musique sans emploi, est nommé surveillant dans un internat de rééducation pour mineurs.
+        Pas à pas, il va tenter d'initier ses élèves au solfège et au chant.`,
+    },
+    {
+        title : "Les 100",
+        image: "images/les100.jpg",
+        text : `Après une apocalypse nucléaire, les 318 survivants se réfugient dans des stations spatiales et
+        parviennent à y vivre et à se reproduire, atteignant le nombre de 4000 ; 97 ans plus tard,
+        une centaine de jeunes délinquants redescendent sur Terre.`,
+    },
+    {
+        title : "Start-up",
+        image : "images/start-up.jpg",
+        text : `Cherchant à réaliser leurs rêves, de jeunes entrepreneurs se lancent dans la création de leurs start-up.
+        Pour ces derniers, intégrer le prestigieux Sand-box, établissement finançant le lancement de nouvelles entreprises,
+        est indispensable. Face à la dure réalité de la vie, Dal-mi se bat pour ne pas regretter les choix du passé.
+        Elle doit désormais affronter sa grande sœur, de retour en Corée du Sud après de longues années passées aux États-Unis.
+        Génie en sciences, Do-San est engagé pour rester aux côtés de Dal-mi lors d’une soirée de réseautage.
+        Celle-ci croit avoir affaire à son premier amour. Immédiatement, des liens se tissent.`,
+    }
+];
